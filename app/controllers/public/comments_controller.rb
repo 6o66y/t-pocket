376c66870099e7b2bcae712e82e019c341aed48a
vagrant <vagrant@localhost.localdomain>
2020-06-06 10:32:01 +0000

    [Add] public/controllers

---
+++ app/controllers/public/comments_controller.rb
@@ -0,0 +1,5 @@
+class Public::CommentsController < ApplicationController
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'comments/edit'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'relationships/following'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
