f5cc69f60f6bb4b97738307703d71d10da3d17d3
vagrant <vagrant@localhost.localdomain>
2020-06-06 10:15:57 +0000

    [Update] db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_094456) do
+ActiveRecord::Schema.define(version: 2020_06_06_100931) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_displayed", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.integer "post_id_id"
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id_id"], name: "index_comments_on_post_id_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.integer "post_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id_id"], name: "index_likes_on_post_id_id"
+    t.index ["user_id_id"], name: "index_likes_on_user_id_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.string "title", null: false
+    t.string "image_id"
+    t.text "body", null: false
+    t.string "objective"
+    t.string "review"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_relationships_on_user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
