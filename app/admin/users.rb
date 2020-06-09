610d08b12833b0ac5a7fb17e95fadcae7d83dd7d
vagrant <vagrant@localhost.localdomain>
2020-06-09 20:02:20 +0000

    [Update] admin/posts.rb & users.rb

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,14 +2,15 @@ ActiveAdmin.register User do
 
   permit_params :name, :username, :introduction, :profile_image, :is_deleted
 
-  # index do
-  #   column :id
-  #   column :name
-  #   column :username
-  #   column :profile_image
-  #   column :introduction
-  #   column :is_deleted
-  #   column :created_at
-  #   column :updated_at
-  # end
+  index do
+    column :id
+    column :name
+    column :username
+    column :profile_image
+    column :introduction
+    column :is_deleted
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
