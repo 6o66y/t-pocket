c0b2e9be1d51521e78207ab7e68422993e907514
vagrant <vagrant@localhost.localdomain>
2020-06-09 19:45:10 +0000

    [Update] scss

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -2,16 +2,16 @@ ActiveAdmin.register Post do
 
   permit_params :title, :objective, :body, :review, :image, :category_id
 
-  index do
-    column :id
-    column :user
-    column :title
-    column :objective
-    column :body
-    column :review
-    column :image
-    column :category_id
-    column :created_at
-    column :updated_at
-  end
+  # index do
+  #   column :id
+  #   column :user
+  #   column :title
+  #   column :objective
+  #   column :body
+  #   column :review
+  #   column :image
+  #   column :category_id
+  #   column :created_at
+  #   column :updated_at
+  # end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,30 +2,14 @@ ActiveAdmin.register User do
 
   permit_params :name, :username, :introduction, :profile_image, :is_deleted
 
-  index do
-    column :id
-    column :name
-    column :username
-    column :profile_image
-    column :introduction
-    column :is_deleted
-    column :created_at
-    column :updated_at
-end
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :name, :, :profile_image_id, :introduction, :is_deleted
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :username, :profile_image_id, :introduction, :is_deleted]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
+  # index do
+  #   column :id
+  #   column :name
+  #   column :username
+  #   column :profile_image
+  #   column :introduction
+  #   column :is_deleted
+  #   column :created_at
+  #   column :updated_at
   # end
-  
 end
