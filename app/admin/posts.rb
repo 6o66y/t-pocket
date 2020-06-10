3747c5b9c064c374d2077425b18af538b48e83b5
vagrant <vagrant@localhost.localdomain>
2020-06-10 12:18:10 +0000

    [Update] 管理者ページ

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Post do
 
-  permit_params :title, :objective, :body, :review, :image, :category_id
+  permit_params :title, :objective, :body, :review, :category_id
 
   index do
     column :id
@@ -9,10 +9,25 @@ ActiveAdmin.register Post do
     column :objective
     column :body
     column :review
-    column :image
     column :category_id
     column :created_at
     column :updated_at
     actions
   end
+
+  show do
+    attributes_table do
+      row :id
+      row :user
+      row :title
+      row :objective
+      row :body
+      row :review
+      row :category_id
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,17 +1,39 @@
 ActiveAdmin.register User do
 
-  permit_params :name, :username, :introduction, :profile_image, :is_deleted, :email
+  permit_params :name, :is_deleted, :email
 
   index do
     column :id
     column :name
     column :username
-    column :profile_image
     column :introduction
     column :email
     column :is_deleted
     column :created_at
     column :updated_at
     actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :is_deleted
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :username
+      row :introduction
+      row :email
+      row :is_deleted
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
   end
 end
