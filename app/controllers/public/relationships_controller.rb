93b9a4836b19d4f41ab3e5744de040b9a8ed4ef6
vagrant <vagrant@localhost.localdomain>
2020-06-11 03:06:29 +0000

    [Making] フォロー機能

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -7,8 +7,16 @@ class Public::RelationshipsController < ApplicationController
   end
 
   def create
+  	@user = User.find(params[:follow_id])
+  	following = current_user.follow(@user)
+  	following.save
+  	redirect_back(fallback_location: posts_path)
   end
 
   def destroy
+  	@user = User.find(params[:follow_id])
+  	following = current_user.unfollow(@user)
+  	following.destroy
+  	redirect_back(fallback_location: posts_path)
   end
 end
--- db/migrate/20200606100120_create_relationships.rb
+++ db/migrate/20200606100120_create_relationships.rb
@@ -2,10 +2,11 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
       t.references :user, foreign_key: true
-      t.integer :follower_id
-      t.integer :following_id
+      t.references :follow, foreign_key: {to_table: :users} # 参照先テーブル:users
 
       t.timestamps
+
+      t.index [:user_id, :follow_id], unique: true # 重複してフォローすることを防ぐ
     end
   end
 end
