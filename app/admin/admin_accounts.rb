47c82075acb6b2e1bada1b7c980ddd86f38a71eb
vagrant <vagrant@localhost.localdomain>
2020-06-15 01:08:34 +0000

    [Update] admin_accounts.rb

--- app/admin/admin_accounts.rb
+++ app/admin/admin_accounts.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register AdminAccount do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :username
 
   index do
     selectable_column
     id_column
     column :email
+    column :username
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -12,6 +13,7 @@ ActiveAdmin.register AdminAccount do
   end
 
   filter :email
+  filter :username
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
@@ -19,6 +21,7 @@ ActiveAdmin.register AdminAccount do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :username
       f.input :password
       f.input :password_confirmation
     end
